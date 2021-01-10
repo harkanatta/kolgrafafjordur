8726c1ff18bcfa6f036e12dacb4fc051faa1e5ab
Valtýr Sigurðsson <harkanatta@users.noreply.github.com>
2021-01-10 20:46:25 +0000

    skjol

---
+++ skissur.R
@@ -0,0 +1,26 @@
+
+
+stodvar <- read.csv("skjol/stodvar.csv",header = T,encoding = "UTF-8",row.names = NULL)
+rass <- make.names(stodvar[, 1], unique=TRUE)
+stodvar <- stodvar[, -1]
+
+stodvar <- stodvar %>% 
+  mutate_all(~na_if(., '#VALUE!')) %>% replace(is.na(.), 0) %>% 
+  filter_all(any_vars(. != 0)) %>% 
+  mutate_if(is.character,as.integer)
+str(stodvar)
+
+rownames(stodvar) <- rass[as.numeric(rownames(stodvar))]
+DT::datatable(stodvar,rownames = T)
+
+
+
+
+stodvar <- read.csv("skjol/stod2015.csv",header = T,encoding = "UTF-8",row.names = NULL)
+rass <- make.names(stodvar[, 1], unique=TRUE)
+stodvar <- stodvar[, -1]
+stodvar <- stodvar %>% filter_all(any_vars(. != 0))
+str(stodvar)
+
+rownames(stodvar) <- rass[as.numeric(rownames(stodvar))] #bull
+DT::datatable(stodvar,rownames = T)
