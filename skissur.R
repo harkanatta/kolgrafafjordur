e82dfb7ee38194138ce663340054868738a2dbb5
Valtýr Sigurðsson <harkanatta@users.noreply.github.com>
2021-01-12 23:49:44 +0000

    smá klunnalegar lagfæringar

--- skissur.R
+++ skissur.R
@@ -1,16 +1,22 @@
-
+library(DT)
+library(tidyverse)
 
 stodvar <- read.csv("skjol/stodvar.csv",header = T,encoding = "UTF-8",row.names = NULL)
 rass <- make.names(stodvar[, 1], unique=TRUE)
 stodvar <- stodvar[, -1]
 
 stodvar <- stodvar %>% 
-  mutate_all(~na_if(., '#VALUE!')) %>% replace(is.na(.), 0) %>% 
-  filter_all(any_vars(. != 0)) %>% 
-  mutate_if(is.character,as.integer)
-str(stodvar)
+  mutate_all(~na_if(., '#VALUE!')) %>% replace(is.na(.), 0) 
 
 rownames(stodvar) <- rass[as.numeric(rownames(stodvar))]
+
+stodvar <- stodvar %>% 
+  filter_all(any_vars(. != 0)) #%>% 
+  #mutate_if(is.character,as.integer)
+str(stodvar)
+
+
+
 DT::datatable(stodvar,rownames = T)
 
 
