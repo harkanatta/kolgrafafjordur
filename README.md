f4bf457cf3893628254ebef8be4d6c2e808c136f
harkanatta <valtyrgullhjarta@gmail.com>
2021-10-27 15:37:36 +0000

    talning

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Kolgrafafjordur
-Botndýrarannsókn í Kolgrafafirði. 
+Botndýrarannsókn í Kolgrafafirði. Greiningar úr sýnum frá 2016.
+Myndir eru geymdar hér: https://www.dropbox.com/home/Biopol/Kolgrafafjordur/greiningar
--- skissur.R
+++ skissur.R
@@ -2,31 +2,55 @@ library(DT)
 library(tidyverse)
 
 stodvar <- read.csv("skjol/stodvar.csv",header = T,encoding = "UTF-8",row.names = NULL)
-rass <- make.names(stodvar[, 1], unique=TRUE)
-stodvar <- stodvar[, -1]
+Names <- make.names(stodvar[, 1], unique=TRUE) # Make a vector of names from the first column
+stodvar <- stodvar[, -1] #delete the first column
 
 stodvar <- stodvar %>% 
-  mutate_all(~na_if(., '#VALUE!')) %>% replace(is.na(.), 0) 
+  mutate_all(~na_if(., '#VALUE!')) %>% replace(is.na(.), 0) %>% #replace "#VALUE!" with zeros
+  mutate_if(is.character,as.integer)
 
-rownames(stodvar) <- rass[as.numeric(rownames(stodvar))]
+rownames(stodvar) <- Names#[as.numeric(rownames(stodvar))] 
 
 stodvar <- stodvar %>% 
-  filter_all(any_vars(. != 0)) #%>% 
-  #mutate_if(is.character,as.integer)
-str(stodvar)
+  filter_all(any_vars(. != 0)) #remove rows with only zeros
 
+#Names <- make.names(rownames(stodvar)) 
+#rownames(stodvar) <- Names#[as.numeric(rownames(stodvar))]
 
-
-DT::datatable(stodvar,rownames = T)
+DT::datatable(stodvar,rownames = T, caption = "2017")
 
 
 
 
 stodvar <- read.csv("skjol/stod2015.csv",header = T,encoding = "UTF-8",row.names = NULL)
-rass <- make.names(stodvar[, 1], unique=TRUE)
+Names <- make.names(stodvar[, 1], unique=TRUE)
 stodvar <- stodvar[, -1]
 stodvar <- stodvar %>% filter_all(any_vars(. != 0))
 str(stodvar)
 
-rownames(stodvar) <- rass[as.numeric(rownames(stodvar))] #bull
-DT::datatable(stodvar,rownames = T)
+rownames(stodvar) <- Names[as.numeric(rownames(stodvar))] #bull
+DT::datatable(stodvar,rownames = T, caption = "2015")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pngfile <- pdftools::pdf_convert('skjol/lykill.pdf', dpi = 600)
+(islenska <- tesseract("isl"))
+text <- tesseract::ocr(pngfile, engine = islenska)
+#cat(text)
+
+#(islenska <- tesseract("isl"))
